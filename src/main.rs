fe69060ca613fb8d3e01c88e82cb77b056402ed0
Andreas Bigger <abigger@nascent.xyz>
2021-05-30 17:38:54 -0400

    lazy statics and spinning locks

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 #![no_std]
 #![no_main]
 
-// extern crate vga_buffer;
-use vga_buffer;
+extern crate vga_buffer;
+
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
@@ -12,17 +12,9 @@ static HELLO: &[u8] = b"Hello World!";
 #[no_mangle]
 /// Default entrypoint for Rust. Overwrites crt0, subverting _start_ lang item
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    // ** Test our vga_buffer module
-    vga_buffer::print_something();
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
     loop {}
 }
